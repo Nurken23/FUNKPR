66159cc8f728a16f5f35a8f1db8a3726e6584d41
Nurken23 <62428942+Nurken23@users.noreply.github.com>
2023-01-24 09:34:39 +0600

    Add files via upload

---
+++ agfs.py
@@ -0,0 +1,23 @@
+#переменные
+
+s = "Kani"
+d = "Nurken"
+f = "Aiboluli"
+
+#ФИО
+
+print(s,d,f)
+print("Age: ",19)
+
+#ввод данных
+
+a = int(input())
+
+#условные операторы
+
+if a==2:
+    print("two")
+elif a<=5:
+    print("Five")
+else:
+    print("NO")
